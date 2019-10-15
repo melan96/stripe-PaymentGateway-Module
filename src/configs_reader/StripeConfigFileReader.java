ddcfa6bfbe9ae17f13bdbc108792ee40e4f6aa8e
melan96 <it17006712@my.sliit.lk>
2019-10-15 07:49:11 +0530

    StripeConfigFileReader added

---
+++ src/configs_reader/StripeConfigFileReader.java
@@ -0,0 +1,18 @@
+package configs_reader;
+
+/**
+ * @author melanrashitha
+ *
+ */
+
+/*
+ * Class StripeConfigFileReader Handles configs.props properties 
+ * Dynamically reads and write configs to props file, manage by StripeModule class
+ * Used additional dependencies 
+ * 		# FileInputStream.io
+ * 		# IOExceptions Handlers
+ */
+
+public class StripeConfigFileReader {
+
+}
