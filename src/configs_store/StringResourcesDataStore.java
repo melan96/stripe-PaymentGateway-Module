b621d9b3992b25c662f60a590dddb2799b1e7493
melan96 <it17006712@my.sliit.lk>
2019-10-16 17:31:06 +0530

    StringResources Files added to manage static resources

---
+++ src/configs_store/StringResourcesDataStore.java
@@ -0,0 +1,13 @@
+package configs_store;
+
+public class StringResourcesDataStore {
+
+    /*
+    *   Class StringResourceDataStore
+    *   This class defines static String Resources of Component
+    *   Default values are sorted as final specifiers
+    *   Class compilation status : false
+    *   StringIterators : true
+     */
+
+}
