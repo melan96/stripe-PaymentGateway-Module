823ddbbe45cf8445ae554e4dbff6311c8e595600
melan96 <it17006712@my.sliit.lk>
2019-10-16 17:46:59 +0530

    module descriptive information added

--- src/configs_store/StringResourcesDataStore.java
+++ src/configs_store/StringResourcesDataStore.java
@@ -8,6 +8,14 @@ public class StringResourcesDataStore {
     *   Default values are sorted as final specifiers
     *   Class compilation status : false
     *   StringIterators : true
-     */
+    */
+
+    //Stripe PaymentGateway Module Version
+    private final String StripeAPIVersion="1.0";
+
+    //Module ID
+    private final String StripeModuleID="1000000000";
+
+    
 
 }
