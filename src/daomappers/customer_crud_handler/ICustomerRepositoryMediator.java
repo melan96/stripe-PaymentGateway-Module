6ea6ff49eabeedc6a9b79426c9f78bfaf215f335
melan96 <it17006712@my.sliit.lk>
2020-01-27 12:04:46 +0530

    Customer CRUD Handlers added to packages

---
+++ src/daomappers/customer_crud_handler/ICustomerRepositoryMediator.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+package daomappers_pkg.repository_crud_mediator__pkg.customer_crud_handler;
+
+/**
+ * @author melanrashitha
+ * @date Sep 24, 2019
+ * @FileName ICustomerRepositoryMediator.java
+ */
+public interface ICustomerRepositoryMediator {
+
+}
