6f21d4ecb1fc72ac2057745d9a1de153d3742327
melan96 <it17006712@my.sliit.lk>
2020-01-27 11:55:15 +0530

    IPayment DataObjectDefinitions added

---
+++ src/daomappers/payment_repo/IPaymentDAO.java
@@ -0,0 +1,11 @@
+package daomappers_pkg.data_definitions__pkg.payment_repo;
+
+/**
+ * @author melanrashitha
+ * @date Sep 21, 2019
+ * @FileName IPaymentDAO.java
+ */
+public interface IPaymentDAO {
+
+
+}
